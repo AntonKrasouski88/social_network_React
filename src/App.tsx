4fe9dee4218ed0c96d572aa6c3fa4b32b4f6c1ad
AntonKrasouski <antohkad21@gmail.com>
2022-12-15 22:50:14 +0300

    Blocks added  on a page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import './App.css';
+import logo from 'img/logo/logo.png'
+
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+    return (
+        <div>
+            <header>
+                <img src="img/logo/logo.png" alt={`logo`}/>
+            </header>
+            <nav>
+                <div>Profile</div>
+                <div>Messages</div>
+                <div>News</div>
+                <div>Music</div>
+            </nav>
+            <div>
+                Main content
+            </div>
+        </div>
+    );
 }
 
 export default App;
