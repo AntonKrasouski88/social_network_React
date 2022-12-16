555272cabafb0078a939e371d2a2ed8c2b40b1b6
AntonKrasouski <antohkad21@gmail.com>
2022-12-16 21:37:34 +0300

    style app.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import './App.css';
-import logo from 'img/logo/logo.png'
+import logo from './img/logo/logo.png'
 
 
 function App() {
     return (
-        <div>
-            <header>
-                <img src="img/logo/logo.png" alt={`logo`}/>
+        <div className={'container'}>
+            <header className={'header'}>
+                <img src={logo} alt={'logo_social_network'}/>
             </header>
-            <nav>
+            <nav className={'nav'}>
                 <div>Profile</div>
                 <div>Messages</div>
                 <div>News</div>
                 <div>Music</div>
             </nav>
-            <div>
+            <div className={'content'}>
                 Main content
             </div>
         </div>
