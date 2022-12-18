1a452464f7ed4b2ded70a9311c5a0ba0371cc8d7
AntonKrasouski <antohkad21@gmail.com>
2022-12-18 13:43:33 +0300

    21 - layout page dialogs

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,8 +3,25 @@ import styleDialogs from './Dialogs.module.css'
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={styleDialogs.container}>
+            <div className={styleDialogs.dialogsPerson}>
+                <div className={styleDialogs.dialogsItem + ' ' + styleDialogs.active}>Kristina</div>
+                <div className={styleDialogs.dialogsItem}>Dima</div>
+                <div className={styleDialogs.dialogsItem}>Lina</div>
+                <div className={styleDialogs.dialogsItem}>Bob</div>
+                <div className={styleDialogs.dialogsItem}>Jan</div>
+            </div>
+            <div className={styleDialogs.litters}>
+                <div className={styleDialogs.message}>
+                    Hi!
+                </div>
+                <div className={styleDialogs.message}>
+                    How are you?
+                </div>
+                <div className={styleDialogs.message}>
+                    I'll call you back!
+                </div>
+            </div>
         </div>
     );
 };
