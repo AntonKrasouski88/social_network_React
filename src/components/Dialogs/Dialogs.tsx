1e6abdf7e44238c4d957423e0f49a641396b0f38
AntonKrasouski88 <antohkad21@gmail.com>
2022-12-21 15:57:25 +0300

    25 - method map for Dialogs component

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,9 +3,8 @@ import styleDialogs from './Dialogs.module.css'
 import DialogsItem from "./DialogItem/DialogsItem";
 import Message from "./Message/Message";
 
-
 const Dialogs = () => {
-    const dialogsData = [
+    const dialogs = [
         {id: 1, name: 'Kristina'},
         {id: 2, name: 'Dima'},
         {id: 3, name: 'Lina'},
@@ -13,7 +12,7 @@ const Dialogs = () => {
         {id: 5, name: 'Jan'},
     ];
 
-    const messageData = [
+    const messages = [
         {id: 1, message: 'Hi!'},
         {id: 2, message: 'How are you'},
         {id: 3, message: 'I will call you back'},
@@ -21,22 +20,13 @@ const Dialogs = () => {
         {id: 5, message: 'YO'},
     ]
 
-
     return (
         <div className={styleDialogs.container}>
             <div className={styleDialogs.dialogsPerson}>
-                <DialogsItem id={dialogsData[0].id} name={dialogsData[0].name}/>
-                <DialogsItem id={dialogsData[1].id} name={dialogsData[1].name}/>
-                <DialogsItem id={dialogsData[2].id} name={dialogsData[2].name}/>
-                <DialogsItem id={dialogsData[3].id} name={dialogsData[3].name}/>
-                <DialogsItem id={dialogsData[4].id} name={dialogsData[4].name}/>
+                {dialogs.map((d) => <DialogsItem id={d.id} name={d.name}/>)}
             </div>
             <div className={styleDialogs.litters}>
-                <Message message={messageData[0].message}/>
-                <Message message={messageData[1].message}/>
-                <Message message={messageData[2].message}/>
-                <Message message={messageData[3].message}/>
-                <Message message={messageData[4].message}/>
+                {messages.map(m => <Message message={m.message}/>)}
             </div>
         </div>
     );
