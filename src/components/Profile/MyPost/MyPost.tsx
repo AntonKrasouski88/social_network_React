7f2be3ddff2cabd0e76bc33aa651a83232594a33
AntonKrasouski88 <antohkad21@gmail.com>
2022-12-21 16:13:27 +0300

    25 - method map for MyPost component

--- src/components/Profile/MyPost/MyPost.tsx
+++ src/components/Profile/MyPost/MyPost.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import styleMyPost from './MyPost.module.css'
 import Post from "./Post/Post";
 
+
 const MyPost = () => {
 
-    const postData = [
+    const posts = [
         {id: 1, message: 'Hello my friend!', countLikes: 5},
         {id: 2, message: 'How are you?', countLikes: 7},
         {id: 3, message: 'Do you look a new movie?', countLikes: 15},
@@ -21,10 +22,7 @@ const MyPost = () => {
                 <div className={styleMyPost.addButtonPost}>
                     <button>Add post</button>
                 </div>
-                <Post messages={postData[0].message} countLikes={postData[0].countLikes}/>
-                <Post messages={postData[1].message} countLikes={postData[1].countLikes}/>
-                <Post messages={postData[2].message} countLikes={postData[2].countLikes}/>
-                <Post messages={postData[3].message} countLikes={postData[3].countLikes}/>
+                {posts.map(p=> <Post message={p.message} countLikes={p.countLikes}/>)}
             </div>
         </div>
     );
--- src/components/Profile/MyPost/Post/Post.tsx
+++ src/components/Profile/MyPost/Post/Post.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import person from '../../../../img/logo/person.png';
 import stylePost from './Post.module.css'
 
-type PostTypeProps = {
-    messages: string;
+export interface PostTypeProps {
+    message: string;
     countLikes: number;
 }
 
@@ -12,7 +12,7 @@ const Post = (props: PostTypeProps) => {
         <div className={stylePost.blockPost}>
             <div className={stylePost.post}>
                 <div><img src={person} alt={'person_logo'}/></div>
-                <div className={stylePost.textPost}>{props.messages}</div>
+                <div className={stylePost.textPost}>{props.message}</div>
             </div>
             <span>{`like ${props.countLikes}`}</span>
         </div>
