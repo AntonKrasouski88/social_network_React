b96872e07936d3b1e9a56dbebb1e81d03617adf3
AntonKrasouski88 <antohkad21@gmail.com>
2023-01-04 09:24:02 +0300

    refactor: Sidebar -  img friends

--- src/components/Navbar/Sidebar/Sidebar.tsx
+++ src/components/Navbar/Sidebar/Sidebar.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+import person from '../../../img/logo/person.png'
+import s from './Sidebar.module.css'
 
 export type SidebarProps = {
     name: string
@@ -6,9 +8,9 @@ export type SidebarProps = {
 
 export const Sidebar = (props: SidebarProps) => {
   return (
-    <div>
-        <div><img src="" alt="" /></div>
-        <div>{props.name}</div>
+    <div className='s.wrapper'>
+        <div className={s.blockImg}><img src={person} alt="img_person" /></div>
+        <div className={s.name}>{props.name}</div>
     </div>
     
   )
